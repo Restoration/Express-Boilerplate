70a66b2aebd07bf25cee68a6270f5221cd1cae00
RyotArch <bboy.ryu@icloud.com>
2019-01-28 19:27:38 -0800

    Fixed layout
    Added express-layouts-ejs

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var  expressLayouts = require('express-ejs-layouts');
+
 
 // DatabaseClass
 /*
@@ -46,6 +48,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 // Passport
 //----------------------------------------------------------------------
