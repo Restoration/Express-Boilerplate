76d5f5f51ee4cbdc1beabeb2818e681d9eec65b5
RyotArch <bboy.ryu@icloud.com>
2019-04-15 10:23:05 -0700

    Created Register

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,7 +7,6 @@ router.get('/', (req, res) => {
 });
 router.post('/create',verifyToken, (req, res) => {
     jwt.verify(req.token, 'secretkey', (err, authData) =>{
-        console.log(err);
         if(err){
             res.sendStatus(403);
         } else {
@@ -18,14 +17,42 @@ router.post('/create',verifyToken, (req, res) => {
         }
     });
 });
+
+
+router.post('/register', (req, res) => {
+    let hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    userObj = {
+        name : req.body.name,
+        email : req.body.email,
+        password : hashedPassword
+    }
+    // Call Database class register method
+    DB.register(userObj,(err, user) => {
+        if (err){
+            res.sendStatus(500);
+        }
+        // 86400 = 24h
+        jwt.sign({user},'secretkey', { expiresIn: 86400 }, (err, token) => {
+            res.json({
+                token
+            });
+        });
+    });
+});
+
 router.post('/login', (req, res) => {
     // Mock user
     const user = {
         id: 1,
         username: 'test',
-        email: 'test@example.com'
+        email: 'test@example.com',
+        password: '',
     }
-    jwt.sign({user},'secretkey', { expiresIn: '1h' }, (err, token) => {
+    let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid){
+        res.sendStatus(403);
+    }
+    jwt.sign({user},'secretkey', { expiresIn: 86400 }, (err, token) => {
         res.json({
             token
         });
