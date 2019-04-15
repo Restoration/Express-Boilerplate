ab6ba5046194751707973f3a8a188478aa9549f0
RyotArch <bboy.ryu@icloud.com>
2019-04-15 10:01:51 -0700

    Fixed Auth code

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const expiresTime = '30s';
 
 router.get('/', (req, res) => {
     res.json({ message: 'Hello Express Boilerplate API' });
 });
-router.post('/api/create',verifyToken, (req, res) => {
+router.post('/create',verifyToken, (req, res) => {
     jwt.verify(req.token, 'secretkey', (err, authData) =>{
+        console.log(err);
         if(err){
             res.sendStatus(403);
         } else {
@@ -18,15 +18,14 @@ router.post('/api/create',verifyToken, (req, res) => {
         }
     });
 });
-router.post('/api/login', (req, res) => {
+router.post('/login', (req, res) => {
     // Mock user
     const user = {
         id: 1,
         username: 'test',
         email: 'test@example.com'
     }
-
-    jwt.sign({user},'secretkey', {expiresIn:expiresTime},(err, token) => {
+    jwt.sign({user},'secretkey', { expiresIn: '1h' }, (err, token) => {
         res.json({
             token
         });
@@ -37,13 +36,7 @@ router.post('/api/login', (req, res) => {
 function verifyToken(req, res, next){
     const bearerHeader = req.headers['authorization'];
     if(bearerHeader !== 'undefined'){
-        // Split at the space
-        const bearer = bearerHeader.split(' ');
-        // Get token from array
-        const bearerToken = bearer[1];
-        // Set the token
-        req.token = bearerToken;
-        // Next middleware
+        req.token = bearerHeader;
         next();
     } else {
         // Forbidden
