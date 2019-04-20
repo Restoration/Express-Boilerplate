8adf4d89dd0e84bff0392cd7a26e3660c1ce450f
RyotArch <bboy.ryu@icloud.com>
2019-04-19 17:24:09 -0700

    Refactoring code

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -19,29 +18,6 @@ router.post('/create',verifyToken, (req, res) => {
     });
 });
 
-
-router.post('/register', (req, res) => {
-    // 86400 = 24h
-    jwt.sign({req.body},'secretkey', { expiresIn: 86400 }, (err, token) => {
-        let hashedPassword = bcrypt.hashSync(req.body.password, 8);
-        userObj = {
-            name : req.body.name,
-            email : req.body.email,
-            password : hashedPassword
-            token : token,
-        }
-        DB.register(userObj,(err, user) => {
-            if (err){
-                res.sendStatus(500);
-            }
-        });
-        storeTokenLS(token);
-        res.json({
-            token
-        });
-    });
-});
-
 router.post('/login', (req, res) => {
     // Mock user
     const user = {
@@ -49,11 +25,6 @@ router.post('/login', (req, res) => {
         username: 'test',
         email: 'test@example.com',
         password: '',
-    }
-    // Check password
-    let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-    if (!passwordIsValid){
-        res.sendStatus(403);
     }
     // Get the token
     jwt.sign({user},'secretkey', { expiresIn: 86400 }, (err, token) => {
@@ -63,6 +34,11 @@ router.post('/login', (req, res) => {
         });
     });
 });
+
+router.get('/logout', (req, res) => {
+    deleteTokenLS();
+});
+
 
 // Verify Token
 function verifyToken(req, res, next){
@@ -83,6 +59,10 @@ function storeTokenLS(token){
         localStorage = new LocalStorage('./scratch');
     }
     localStorage.setItem('token', token);
+}
+// Delete token to local storage
+function deleteTokenLS(){
+    localStorage.removeItem('token');
 }
 
 module.exports = router;
